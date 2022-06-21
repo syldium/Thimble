bee871db4a3eb233dbfe3ab0b910b6ec9360d5eb
syldium <syldium@mailo.com>
2022-06-21 18:15:22 +0200

    change(sql): use VARCHAR for usernames
    Most usernames have less than 16 characters and H2 databases pad right char strings that are too short.

--- common/src/main/resources/sql/tables-mysql.sql
+++ common/src/main/resources/sql/tables-mysql.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS thimble_players
 (
-    uuid     CHAR(36) NOT NULL,
-    name     CHAR(16) NOT NULL,
-    wins     INTEGER  NOT NULL,
-    losses   INTEGER  NOT NULL,
-    jumps    INTEGER  NOT NULL,
-    fails    INTEGER  NOT NULL,
-    thimbles INTEGER  NOT NULL,
+    uuid     CHAR(36),
+    name     VARCHAR(16) NOT NULL,
+    wins     INTEGER     NOT NULL,
+    losses   INTEGER     NOT NULL,
+    jumps    INTEGER     NOT NULL,
+    fails    INTEGER     NOT NULL,
+    thimbles INTEGER     NOT NULL,
     PRIMARY KEY (uuid)
 );
--- common/src/main/resources/sql/tables-postgre.sql
+++ common/src/main/resources/sql/tables-postgre.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS thimble_players
 (
-    uuid     UUID     NOT NULL,
-    name     CHAR(16) NOT NULL,
-    wins     INTEGER  NOT NULL,
-    losses   INTEGER  NOT NULL,
-    jumps    INTEGER  NOT NULL,
-    fails    INTEGER  NOT NULL,
-    thimbles INTEGER  NOT NULL,
+    uuid     UUID,
+    name     VARCHAR(16) NOT NULL,
+    wins     INTEGER     NOT NULL,
+    losses   INTEGER     NOT NULL,
+    jumps    INTEGER     NOT NULL,
+    fails    INTEGER     NOT NULL,
+    thimbles INTEGER     NOT NULL,
     PRIMARY KEY (uuid)
 );
